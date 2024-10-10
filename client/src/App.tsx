b53404d2609030e1b4d24562d9c513622a754e6f
tylerwong1 <tjwong1@gmail.com>
2024-10-10 17:02:17 -0400

    added routing and pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+// import Home from "./components/Home";
+// import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+
+import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      {/* <Route path="/login" element={<Login />} /> */}
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
